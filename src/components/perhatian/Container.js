58a6a18e0b7a68d56c2ec2ce3df8f581ef5de5eb
regita <cregita3@gmail.com>
2022-10-18 09:29:09 +0700

    tambah tket

---
+++ src/components/perhatian/Container.js
@@ -0,0 +1,6 @@
+export default function Container ({}) {
+    return (
+        <div className='container bg-gray-100 p-10 h-screen'>
+        </div>
+    );
+}
--- src/components/tiket/Content.js
+++ src/components/tiket/Content.js
@@ -2,10 +2,65 @@ import React from 'react'
 
 function Content() {
   return (
-    <div className='h-72 m-auto'>
-        <p className='pt-32'> INI TIKET</p>
+  
+    // className="items-center flex flex-wrap bg-[#A70B0B]">
+
+      <div className>
+        <button class="ml-96 mt-10 bg-[#A70B0B] hover:bg-[#A70B0B]  text-white font-bold py-4 px-8 rounded-full focus:outline-none focus:shadow-outline">
+        Download
+        </button>
+      <div className=" mx-1 z-10 w-[80%]">
+        
+      </div>
+        <div className="items-center flex flex-wrap bg-[#A70B0B] mt-14 pt-32pb-20">
+          
+          <div className="w-auto px-10 ml-auto mr-auto text-center">
+            <div className="text-gray-200">
+              <p color="gray-200">
+                Museum Keris Nusantara
+              </p>
+            </div>
+          </div>
+
+          <div className="w-auto px-4 ml-auto mr-auto text-center">
+            <div className="text-gray-200">
+              <p color="gray-200">
+                Museum Keris Nusantara
+              </p>
+            </div>
+          </div>
+
+          <div className="w-auto px-4 ml-auto mr-auto text-center">
+            <div className="text-gray-200">
+              <p color="gray-200">
+                Museum Keris Nusantara
+              </p>
+            </div>
+          </div>
+
+          <div className="w-auto px-4 ml-auto mr-auto text-center">
+            <div className="text-gray-200">
+              <p color="gray-200">
+                Museum Keris Nusantara
+              </p>
+            </div>
+          </div>
+
         </div>
+      </div>
   )
 }
 
 export default Content
+
+
+{/* <div>
+<div className=" pt-24 pb-16 flex content-center items-center justify-center w-full  h-1/3">
+    <div className="bg-gray-100 bg-center absolute top-6 w-[80%] h-1/6  bg-inputdata-backgroung  " />       
+    <div className=" mx-auto z-20 w-[80%] -mt-5">
+        <div className="items-center flex flex-wrap bg-[#A70B0B] mt-2 rounded-b-xl pb-1">
+            <div className="w-full  px-3 ml-auto mr-auto text-center">
+                <div className="text-gray-100">
+                    <p className='text-sm mt-5' color="gray-200">
+                    Solo merupakan salah satu kota yang sering dikunjungi oleh para wisatawan. Tempat wisata yang ada di Solo juga sangat beragam. Salah satu tempat wisata yang ada di Solo yaitu Museum. Banyak museum yang terdapat di Solo karena Solo juga merupakan daerah yang memiliki sejarah yang melimpah.
+                    </p> */}
