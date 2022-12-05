bb07d37358afadd8ae916df50988d1c77d25801b
sabilahnr <sabilahnr@student.uns.ac.id>
2022-12-05 14:36:28 +0700

    tiket dinamik

--- src/components/pembayaran/WorkingSection.js
+++ src/components/pembayaran/WorkingSection.js
@@ -1,22 +1,54 @@
 import axios, { Axios } from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
+import { data } from 'autoprefixer';
 import swal from 'sweetalert';
-import qris from '../../assets/img/pembayaran/Qris.png'
-import tunai from '../../assets/img/pembayaran/Tunai.png'
-import panah from '../../assets/img/pembayaran/panah.png'
+// import qris from '../../assets/img/pembayaran/Qris.png'
+// import tunai from '../../assets/img/pembayaran/Tunai.png'
+// import detail from '../../assets/img/pembayaran/detail.png'
+// import identitas from '../../assets/img/pembayaran/identitas.png'
+// import pilih from '../../assets/img/pembayaran/pilih.png'
+// import total from '../../assets/img/pembayaran/total.png'
 import 'flowbite';
 
 function WorkingSection(input) {
     
     // const data = data;
     const [pembayaran,setPembayaran] = useState('')
+    const [danger,setDanger] = useState('')
     const history = useHistory();
+
+    // tambahan dari form input
+   
+    // const [input,setInput] = useState({
+    //     museum : '',
+    //     category : '',
+    //     calender : '',
+    //     nama : '',
+    //     kota : '',
+    //     phone : '',
+    //     jumlah : '',
+    //     tanggal : '',
+    //     harga_awal : '',
+    //     pembayaran : '',
+    //     status : ''
+        
+    // })
+
+    // end tambahan dari form input
+    
+    const redirect = useHistory();
+
+    // const handleInput =(e) =>{
+    //     setInput({...input,[e.target.name]:e.target.value}) 
+    //     console.log(e);
+    // }
 
     console.log(input);
     const handleCash = () =>{
         // console.log(input.data);
-        setPembayaran('Tunai')
+        if(pembayaran !== '' || input.input == 0){
+            setPembayaran('Tunai')
         console.log(pembayaran);
 
         const data = {
@@ -29,9 +61,12 @@ function WorkingSection(input) {
             tanggal:input.data.tanggal,
             foto:input.data.foto,
             harga_awal:input.input,  
-            pembayaran: 'Tunai',
+            pembayaran: 'cash',
+            // tiket: input.data.kode_tiket,
+            status: 'belum lunas',
         }
 
+        console.log(data);
         // swal({
         //     title: "Anda yakin Pembayaran cash ?",
         //     text: "Pembayaran akan dilakukan secara Cash!",
@@ -77,14 +112,19 @@ function WorkingSection(input) {
                     if(res.data.status === 200)
                     {
                         console.log('MANTAB BERHASIL');
-                        history.push('/tiket');
+                        // history.push('/tiket');
+                        history.push({ pathname:"/tiket",
+                        state : data
+                             });
                     }
                     else if(res.data.status === 422)
                     {
                         console.log('ada yang salah di BE');
                     }
                 });
-
+        }else{
+            setDanger("Silahkan pilih pembayaran terlebih dahulu")
+        }
 
     }
 
@@ -109,14 +149,14 @@ function WorkingSection(input) {
   return (
     <div>
 
-        <div className='container p-8 flex mx-auto justify-center bg-gray-100'>
+        <div className=' xl:px-32 lg:px-10 px-4  lg:flex   justify-center bg-gray-100'>
 
-            <div className='w-2/3 my-12 mx-4'>
-                <div className='container flex bg-white shadow-xl rounded-2xl py-6'>
-                    <div className='w-1/6 '>
-                        
+            <div className='lg:w-2/3 w-full my-2 mt-6 mx-4'>
+                <div className='container mx-auto flex bg-white   rounded-2xl py-4'>
+                    <div className='w-1/6 flex mx-auto justify-center'>
+                    <img src={identitas}  className='w-16 h-16'/>
                     </div>
-                    <div className='w-5/6 justify-around'>
+                    <div className='w-5/6 justify-around '>
                         <p className='my-5 font-bold text-3xl'>Identitas diri</p>
                         <div className='flex'>
                             <div className='font-bold'>
@@ -127,7 +167,7 @@ function WorkingSection(input) {
                                 <p className='my-3'>{input.data.nama}</p>
                                 <p className='mb-3'>{input.data.phone}</p>
                             </div>
-                            <div className='pl-24 font-bold'>
+                            <div className='md:pl-24 font-bold'>
                                 {/* <p>NIK</p> */}
                                 <p className='my-3 '>Asal Kota : </p>
                             </div>
@@ -140,37 +180,28 @@ function WorkingSection(input) {
 
                 </div>
 
-                <div className='container flex bg-white my-12 shadow-xl rounded-2xl py-6'>
-                    <div className='w-1/6 '>
-                        
-                    </div>
-                    <div className='w-5/6'>
-                        <p className='my-5 font-bold text-3xl'>Total Pemmbayaran</p>
-                        <div className='w-full text-right pr-12'>
-                            <p className='my-5 font-bold text-3xl'>{rupiah(input.input)} -</p>
-                        </div>
-                    </div>
-                </div>
+               
 
-                <div className='container flex bg-white my-12 shadow-xl rounded-2xl py-6'>
-                    <div className='w-1/6 '>
+                {input.input == 0 ? "":<div className='container mx-auto flex bg-white my-6   rounded-2xl py-4'>
+                    <div className='w-1/6 flex mx-auto justify-center '>
+                    <img src={pilih}  className='w-16 h-16'/>
                         
                     </div>
                     <div className='w-5/6 ' >
                         <p className='font-bold text-3xl py-4'>Pilih Metode Pembayaran</p>
                         <div className='w-full mx-auto justify-center'>
                             <label className='flex max-w-full items-center hover:bg-gray-100 p-5 mx-5 rounded-3xl my-5 transition-all duration-500 ease-in-out'>
-                                <div className='flex w-2/3 justify-around items-center p-4'>
-                                    <img src={tunai}  className='w-1/4 '/>
+                                <div className='flex w-2/3 justify-around items-center'>
+                                    <img src={tunai}  className='w-1/5 '/>
                                     <p className='text-center text-2xl'>  Tunai </p>
                                 </div>
-                                <input  type="radio" value="Tunai" checked={pembayaran === "Male"}  onChange={onValueChange}
+                                <input  type="radio" value="tunai" checked={pembayaran === "Male"}  onChange={onValueChange}
                                 className ='ml-24 w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
                                 />
                             </label>
                             <label  className='cursor-not-allowed flex max-w-full items-center hover:bg-gray-100 p-5 mx-5 rounded-3xl my-5 transition-all duration-500 ease-in-out'>
-                                <div className='flex w-2/3 justify-around items-center p-4'>
-                                    <img src={qris}  className='w-1/4'/>
+                                <div className='flex w-2/3 justify-around items-center'>
+                                    <img src={qris}  className='w-1/5'/>
                                     <p className='text-center text-2xl'>  Qris </p>
                                 </div>
                                 <input disabled={true} type="radio" value="qris" checked={pembayaran === "Female"} onChange={onValueChange}
@@ -178,108 +209,80 @@ function WorkingSection(input) {
                                 />
                             </label>
                         </div>
-                        <div className='flex justify-end'>
-                            <div className='w-1/2 flex'>
-                                <p className='p-6 pr-12 font-bold text-xl'>Pembayaran : </p>
-                                <p className='p-6 pr-12 font-bold text-xl'>{pembayaran}</p>
+                        <div className='flex flex-col justify-end content-end items-end '>
+                            <div className='lg:w- pr-6 flex'>
+                                <p className='p-3  font-bold text-xl'>Pembayaran : </p>
+                                <p className='p-3  font-bold text-xl'>{pembayaran}</p>
                             </div>    
+                            <p className='pr-6 text-red-500 text-sm'>{"* " + danger}</p>
                         </div>
                     </div>
                     <div>
                     </div>
-                </div>
+                </div>}
                 
-                {/* <div className=' container bg-white mt-12 '>
-                    <p className='font-bold text-xl'>Total :</p>
-                    <p className='text-6xl font-bold'>Rp. {input.input}</p>
-                </div>
-                <div class="flex flex-col px-8 bg-white py-12">
-                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8 ">
-                    <div class="overflow-hidden">
-                        <table class="min-w-full text-center bg-white ">
-                        <thead class="border-b">
-                            <tr>
-                            <th scope="col" colSpan={2} class=" text-sm font-medium border-8 border-red-600 bg-red-700 text-gray-900 px-6 py-4">
-                               <p className='font-bold text-lg text-white text-left'> Pilih Metode Pembayaran</p>
-                            </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="border-gray-500 border-4">
-                                <td class="felx text-sm  text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                                    <div className='flex justify-around items-center p-4'>
-                                        <img src={tunai}  className='w-1/6 '/>
-                                        <p className='text-center text-2xl'>  Tunai </p>
-                                        <img src={panah} className='h-12 align-middle'/>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr class="border-gray-500 border-4">
-                                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                                <div className='flex justify-around items-center p-4'>
-                                        <img src={qris}  className='w-1/6'/>
-                                        <p className='text-center text-2xl'>  Qris </p>
-                                        <img src={panah} className='h-12  align-middle'/>
-                                    </div>
-                                </td>
-                            </tr>
-                            
-                        </tbody>
-                        </table>
-                        </div>
-                    </div>
-                </div>
-                   
-                </div> */}
             
             </div>
-            
-            <div className='w-1/3 my-12 mx-4'>
-                <div className='container flex bg-white shadow-xl rounded-2xl py-6'>
-                    <div className='w-1/6'></div>
-                    <div className='bg-white w-2/3 mx-8'>
-                        <p className='my-5 font-bold text-3xl'>Detail Pesanan</p>
-                        <p>Pesanan Tiket masuk pada</p>
-                        <p>{input.data.museum}</p>
-                        <div className='flex'>
-                            <img />
-                            <div>
-                                <p>Tanggal Pemesanan</p>
-                                <p>{input.data.tanggal}</p>
-                            </div>
+                <div className='lg:w-1/3 w-full my-2 mx-4 mt-6 h-1/2'>
+                    <div className='container  mx-auto flex bg-white   rounded-2xl py-6'>
+                        <div className='w-1/6 min-w-max flex mx-auto justify-center '>
+                            <img src={detail}  className='w-16 h-16'/>
                         </div>
-                        <div className='flex'>
-                            <img />
-                            <div>
-                                <p>Kategori</p>
-                                <p>{input.data.kategori}</p>
+                        <div className='lg:block  bg-white w-5/6 mx-4'>
+                            <p className='my-5 font-bold text-3xl w-full'>Detail Pesanan</p>
+                            <div className='flex flex-wrap'>
+                                <div className='lg:w-full w-1/2'>
+                                    <p className='font-bold'>Pesanan Tiket pada</p>
+                                    <p  >{input.data.museum}</p>
+                                </div>
+                                <div className='w-1/2 lg:w-full lg:pt-4'>
+                                    <div>
+                                        <p className='font-bold'>Tanggal Pemesanan</p>
+                                        <p>{input.data.tanggal}</p>
+                                    </div>
+                                </div>
+                                <div className='w-1/2 mt-4 lg:w-full ' >
+                                    <div>
+                                        <p className='font-bold'>Kategori</p>
+                                        <p>{input.data.kategori}</p>
+                                    </div>
+                                </div>
+                                <div className='w-1/2 mt-4 lg:w-full '>
+                                    <div>
+                                        <p className='font-bold'>Jumlah Tiket</p>
+                                        <p>{input.data.jumlah}</p>
+                                    </div>
+                                </div>
                             </div>
+                            {/* <div className='flex'>
+                                <img />
+                                <div>
+                                    <p>Harga satuan</p>
+                                    <p>{input.data.tanggal}</p>
+                                </div>
+                            </div> */}
                         </div>
-                        <div className='flex'>
-                            <img />
-                            <div>
-                                <p>Jumlah Tiket</p>
-                                <p>{input.data.jumlah}</p>
-                            </div>
+                    </div>
+                    <div className='container mx-auto flex bg-white my-6   rounded-2xl py-4'>
+                        <div className='w-1/6 min-w-max flex mx-auto justify-center '>
+                            <img src={total}  className='w-16 h-16'/>            
                         </div>
-                        <div className='flex'>
-                            <img />
-                            <div>
-                                <p>Harga satuan</p>
-                                {/* <p>{input.data.tanggal}</p> */}
+                        <div className='w-5/6'>
+                            <p className='my-5 font-bold mx-4 text-2xl'>Total Pembayaran</p>
+                            <div className='w-full text-right pr-12'>
+                                <p className='my-5 font-semibold text-2xl'>{rupiah(input.input)} -</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-        </div>
-        <div className='bg-gray-100 flex justify-center'>
             
-            <button className='bg-[#A70B0B] rounded-full py-4 px-8 text-white tracking-wider font-semibold' onClick={handleCash}>Selanjutnya</button>
+
 
         </div>
+                <div className='bg-gray-100 flex justify-center items-end '>
+                    
+                    <button className='bg-[#A70B0B] rounded-full w-full py-4 lg:mx-72 mx-24 text-white tracking-wider font-semibold' onClick={handleCash}>Selanjutnya</button>
+                </div>
     </div>
   )
 }
--- src/components/tiket/Content.js
+++ src/components/tiket/Content.js
@@ -1,11 +1,249 @@
-import React from 'react'
+import axios, { Axios } from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
+import qr from '../../assets/img/tiket/qr.png'
+import 'flowbite';
+import { useLocation } from 'react-router-dom'
+import FormInput from 'components/inputdata/FormInput';
 
-function Content() {
+function Content(input) {
+
+  // const data = data;
+  const [pembayaran,setPembayaran] = useState('')
+  const [danger,setDanger] = useState('')
+  const history = useHistory();
+
+  // dari input data
+  const stateParamVal = useLocation().state
+  console.log(stateParamVal,"ini di tiket");
+  const id_category = stateParamVal.category
+  const id_museum = stateParamVal.museum
+  // const id_nama = stateParamVal.nama
+  // const id_kota = stateParamVal.kota
+  // const id_phone = stateParamVal.phone
+  // const id_jumlah = stateParamVal.jumlah
+  // const id_tanggal = stateParamVal.tanggal
+  // const id_foto = stateParamVal.foto
+  // const id_harga_awal = stateParamVal.input
+ 
+
+  const [data,setData] = useState();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+
+  // useEffect(() => {
+  //   const fetchData = async () =>
+  //   {
+
+      const dataId= {
+        id_category:id_category,
+        id_museum:id_museum,
+      }
+
+      // const resHarga = await fetch(`http://localhost:8000/api/show_harga/${id_category}`, dataId);
+      // const res = await resHarga.json()
+      
+      // const resData = await axios.post(`http://localhost:8000/api/show_data/${id_category}`, dataId)
+      //                         .then(res=>{ console.log(res); setData(res.data.data[0]); })
+      // // console.log(await harga)
+
+  //   }
+
+  //   fetchData();
+  // }, [])
+
+  // end
+  // const [loading, setLoading] = useState(true);
+  // const [pengunjung, setPengunjung] = useState([]);
+//   const [input,setInput] = useState({
+//     nama: '',
+//     kota: '',
+//     phone: '',
+//     jumlah: '',
+//     museum: '',
+//     kategori: '',
+//     tanggal: '',
+//     foto: '',
+// })
+
+  console.log(input);
+  const handleCash = () =>{
+    // console.log(input.data);
+    if(pembayaran !== '' || input.input == 0){
+        setPembayaran('Tunai')
+    console.log(pembayaran);
+
+//   const data = {
+//     nama:input.data.nama,
+//     kota:input.data.kota,
+//     phone:input.data.phone,
+//     jumlah:input.data.jumlah,
+//     museum:input.data.museum,
+//     kategori:input.data.kategori,
+//     tanggal:input.data.tanggal,
+//     foto:input.data.foto, 
+//     pembayaran: 'cash',
+//     status: 'belum lunas',
+// }
+
+// console.log(data);
+
+
+
+  
+
+axios.post(`http://localhost:8000/api/add-pengunjung`, data).then(res => {
+          console.log(res.data);
+          if(res.data.status === 200)
+          {
+              console.log('MANTAB BERHASIL');
+              history.push('/tiket');
+          }
+          else if(res.data.status === 422)
+          {
+              console.log('ada yang salah di BE');
+          }
+      });
+      }else{
+      setDanger("Silahkan pilih pembayaran terlebih dahulu")
+      }
+
+    }
+
+    useEffect(() => {
+
+
+    }, [pembayaran])
+
+        const rupiah = (number)=>{
+          return new Intl.NumberFormat("id-ID", {
+          style: "currency",
+          currency: "IDR"
+          }).format(number);
+        }
+
+        const onValueChange = (e) => {
+          // setPembayaran(e)
+          setPembayaran(e.target.value)
+        }
+
+
+    //   useEffect(() => {
+
+    //     axios.get(`/api/pengunjung`).then(res=>{
+    //         if(res.status === 200)
+    //         {
+    //             setPengunjung(res.data.pengunjung)
+    //             setLoading(false);
+    //         }
+    //     });
+
+    // }, []);
+    
+    // const rupiah = (number)=>{
+    //   return new Intl.NumberFormat("id-ID", {
+    //     style: "currency",
+    //     currency: "IDR"
+    //   }).format(number);
+    // }
+
+    // if(loading)
+    // {
+    //     return <h4>Loading Tiket...</h4>
+    // }
+    // else
+    // {
+    //     var pengunjung_HTMLTABLE = "";
+       
+    //     pengunjung_HTMLTABLE = pengunjung.map( (item, index) => {
+    
   return (
-    <div className='h-72 m-auto'>
-        <p className='pt-32'> INI TIKET</p>
+  
+    // className="items-center flex flex-wrap bg-[#A70B0B]">
+
+      <div className='container mx-auto justify-center'>
+        <div className='flex justify-center'>
+          <button class=" mt-10 bg-[#A70B0B] hover:bg-[#A70B0B]  text-white font-bold py-4 px-8 rounded-full focus:outline-none focus:shadow-outline">
+            Download
+          </button>
         </div>
-  )
+
+        <div className="  bg-[#A70B0B] mt-14 pt-12 pb-1 mx-32">
+
+          <div className="w-auto px-10 ml-auto mr-auto text-center">
+            <div className="text-gray-200">
+              <p className='text-3xl font-bold'>
+                UPT Museum
+              </p>
+            </div>
+          </div>
+
+          <div className="w-auto px-4 ml-auto mr-auto text-center">
+            <div className="text-gray-200">
+              <p className='my-3 text-lg'>
+              ET 0987689
+              </p>
+            </div>
+          </div>
+
+          <div className="flex text-white">
+
+            <div className='w-1/3 px-12'>
+
+              <div className='my-3'>
+                <p className=' text-lg'>Tujuan Wisata</p>
+                <p className='text-xl font-bold'>{stateParamVal.museum}</p>
+              </div>
+
+              <div className='my-3' >
+                <p className=' text-lg'>Tanggal Pemesanan</p>
+                <p  className='text-xl font-bold'>{stateParamVal.tanggal}</p>
+              </div>
+
+              <div className='my-3'>
+                <p className=' text-lg'>Atas Nama</p>
+                <p  className='text-xl font-bold'>{stateParamVal.nama}</p>
+              </div>
+
+            </div>
+
+            <div className='w-1/3'>
+
+              <div className='my-3'>
+                <p className=' text-lg'>Jumlah Pengunjung</p>
+                <p  className='text-xl font-bold'>{stateParamVal.jumlah}</p>
+              </div>
+
+              <div  className='my-3'>
+                <p className=' text-lg'>Status Pembayaran</p>
+                <p  className='text-xl font-bold'>{stateParamVal.status}</p>
+              </div>
+
+            </div>  
+
+            <div className='w-1/3 items-center justify-center mx-auto'>
+              <p className='text-center text-xl font-semibold my-1'>Scan QR code</p>
+              <img src={qr} className="mx-auto w-[40%] "/>
+              <p className='text-center text-3xl font-semibold py-4'>{rupiah(stateParamVal.harga_awal)} -</p>
+            </div>
+            
+          </div>
+
+          <div className="w-full mx-auto text-center text-white my-12">
+            <p className='text-gray-300'>Datang sesuai tanggal pada tiket!</p>
+            <p>Harap simpan tiket ini untuk ditunjukkan di lokasi museum!</p>
+          </div>
+
+        </div>
+
+      </div>
+    )
+  
 }
 
 export default Content
+
+
