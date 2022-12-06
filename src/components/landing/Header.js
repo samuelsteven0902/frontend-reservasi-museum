b9f0c294886cdc0001e14ecfaac321643ba5c1d7
regita <cregita3@gmail.com>
2022-12-06 14:46:03 +0700

    modify header landing

--- src/components/landing/Header.js
+++ src/components/landing/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
     return (
     <> 
         <div className="relative pt-24 pb-32 flex content-center items-center m justify-center h-screen">
-            <div className="bg-landing-background bg-fixed bg-center absolute w-11/12 h-96 rounded-[2rem] m-20" />
+            <div className="bg-landing-background bg-cover bg-[#ECE3DE] bg-center absolute w-11/12 h-96 rounded-[2rem] m-20" />
             <div className="container max-w-8xl relative mx-auto">
                 <div className="items-start flex flex-wrap">
                     <div className="w-full lg:w-6/12 px-0 ml-24 mr-auto text-left">
