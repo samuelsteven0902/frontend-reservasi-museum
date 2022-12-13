24420dd04b18ed23ee3133308db296706dccc0a6
rozyfatmala <rozyfatmala771@gmail.com>
2022-12-13 16:02:00 +0700

    info

--- src/components/superadmin/Cards/Info/InfoAbout.js
+++ src/components/superadmin/Cards/Info/InfoAbout.js
@@ -156,6 +156,8 @@ return (
     <div className='justify-between'>
         <div class="flex space-x-2 justify-between">
             <h3 class="font-merriweather font-bold text-2xl text-gray-600">About</h3>
+            <button type="button" class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#tambahAbout">Tambahkan About
+            </button>
             
         </div>
     </div>
