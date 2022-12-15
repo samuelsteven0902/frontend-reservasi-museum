d0d3aa554d02aa67d8ad78804229634b821b68ce
regita <cregita3@gmail.com>
2022-12-15 11:56:20 +0700

    tambah landing informasi

--- src/info/Informasi.js
+++ src/info/Informasi.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import faq from '../assets/img/Landing/informasi/faq.png';
 import ht from '../assets/img/Landing/informasi/harga tiket.png';
 import pt from '../assets/img/Landing/informasi/panduan.png';
+import ct from '../assets/img/Landing/informasi/cari tiket.png';
 
 function Informasi() {
 	const { t } = useTranslation();
@@ -18,23 +19,23 @@ function Informasi() {
 
             </div>
             <div className='sm:flex block justify-center pt-16 w-full'>
-                {/* <Link to='/panduan'><button className='bg-[#A70B0B] w-56 h-28 py-7 px-4 text-xl text-white border-white border-4 shadow-2xl font-semibold hover:bg-red-700 transition-all duration-500 '>PANDUAN<br></br> PEMESANAN TIKET</button> </Link>  */}
-                {/* <Link to='/Faq'><button className='bg-[#A70B0B] w-56 h-28 py-7 px-4 text-xl text-white border-white border-4 shadow-2xl rounded-lg font-semibold hover:bg-red-700 transition-all duration-500 '>FAQ</button></Link> */}
-                <Link to='/panduan' className='text-center w-72 border-0 transition-all duration-200 ease-in-out rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up"
-     data-aos-delay="150" aos-duration="1000"> 
+                <Link to='/panduan' className='text-center w-72 border-0 transition-all duration-200 ease-in-out rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up"data-aos-delay="150" aos-duration="1000"> 
                 <img src={pt} className="w-24 h-24 mx-auto mb-3"/>
                   <p className='tracking-wide text-lg font-extrabold font-nunito pt-auto text-[#A70B0B]'>{t('landing.panduan.judul')}</p>
                   <p className='tracking-wide text-sm font-medium font-nunito pt-auto text-black '>{t('landing.panduan.desc')}</p>
                 </Link>
-                <Link to='/faq' className='text-center w-72 border-0 transition-all duration-200 ease-in-out mx-12 rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up"
-     data-aos-delay="300" aos-duration="1000"> 
+                <Link to='/faq' className='text-center w-72 border-0 transition-all duration-200 ease-in-out mx-12 rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up"data-aos-delay="300" aos-duration="1000"> 
                 <img src={faq} className="w-24 h-24 mx-auto mb-3 "/>
                   <p className='tracking-wide text-lg font-extrabold font-nunito pt-auto text-[#A70B0B]'>FAQ</p>
                   <p className='tracking-wide text-sm font-medium font-nunito pt-auto text-black'>{t('landing.faq')}</p>
                 </Link>
-                <Link to='/harga-tiket' className='text-center w-72 border-0 transition-all duration-200 ease-in-out  rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up" 
-     data-aos-delay="450" aos-duration="1000"> 
+                <Link to='/harga-tiket' className='text-center w-72 border-0 transition-all duration-200 ease-in-out  rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up" data-aos-delay="450" aos-duration="1000"> 
                 <img src={ht} className="w-24 h-24 mx-auto mb-3"/>
+                  <p className='tracking-wide text-lg font-extrabold font-nunito pt-auto text-[#A70B0B]'>{t('landing.harga.judul')}</p>
+                  <p className='tracking-wide text-sm font-medium font-nunito pt-auto text-black'>{t('landing.harga.desc')}</p>
+                </Link>
+                <Link to='/' className='text-center w-72 border-0 transition-all duration-200 ease-in-out rounded-2xl hover:bg-none sm:hover:bg-[#ECE3DE] p-7' data-aos="fade-up" data-aos-delay="450" aos-duration="1000"> 
+                <img src={ct} className="w-24 h-24 mx-auto mb-3"/>
                   <p className='tracking-wide text-lg font-extrabold font-nunito pt-auto text-[#A70B0B]'>{t('landing.harga.judul')}</p>
                   <p className='tracking-wide text-sm font-medium font-nunito pt-auto text-black'>{t('landing.harga.desc')}</p>
                 </Link>
